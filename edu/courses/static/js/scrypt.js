99c325fdb770f43ca292b777ee8a2e3f31310017
oeananas <oeananas@mail.ru>
2019-09-29 21:24:58 +0300

    edit code examples

--- edu/courses/static/js/scrypt.js
+++ edu/courses/static/js/scrypt.js
@@ -7,11 +7,11 @@ $(function () {
   $("#typing").typed({
     strings: [
       "print('Hello, world!')",
-      "return a * b",
-      "def get_data(self):<br/>&emsp;return self.data",
-      "class Product:<br/>&emsp;pass",
-      "def __init__(self, name):<br/>&emsp;self.name = name",
-      "for i in range(10):<br/>&emsp;print(i ** 2)",
+      "func = lambda a, b: a * b",
+      "def get_data(self):<br/>&nbsp;&nbsp;&nbsp;&nbsp;return self.data",
+      "class Product:<br/>&nbsp;&nbsp;&nbsp;&nbsp;pass",
+      "def __init__(self, name):<br/>&nbsp;&nbsp;&nbsp;&nbsp;self.name = name",
+      "for i in range(10):<br/>&nbsp;&nbsp;&nbsp;&nbsp;print(i ** 2)",
     ],
     typeSpeed: 5,
     backDelay: 1500,
